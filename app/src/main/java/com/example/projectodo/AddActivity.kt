16a5b5140e0e88339f3fbd0843dedd2213ef29e9
bomingming <byj060362@gmail.com>
2023-04-04 17:36:30 +0900

    프로젝트 기간 DatePicker 이벤트 구현

--- app/src/main/java/com/example/projectodo/AddActivity.kt
+++ app/src/main/java/com/example/projectodo/AddActivity.kt
@@ -1,8 +1,10 @@
 package com.example.projectodo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.projectodo.databinding.ActivityAddBinding
+import java.util.Calendar
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,27 @@ class AddActivity : AppCompatActivity() {
         val binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var startDateString = "" // 시작일
+        var endDateString = "" // 마감일
 
+        // 시작일 버튼 클릭 이벤트
+        binding.startDateBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                startDateString = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                binding.startDateText.setText(startDateString)
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        // 마감일 버튼 클릭 이벤트
+        binding.endDateBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                endDateString = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                binding.endDateText.setText(endDateString)
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
 }
