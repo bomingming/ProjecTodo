f38480b06273c2b53023e7e5e6e8ef656d3389f8
bomingming <byj060362@gmail.com>
2023-04-05 15:17:27 +0900

    BottomSheet 삭제 이벤트 구현

--- app/src/main/java/com/example/projectodo/BottomSheetFragment.kt
+++ app/src/main/java/com/example/projectodo/BottomSheetFragment.kt
@@ -1,17 +1,41 @@
 package com.example.projectodo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.projectodo.databinding.FragmentBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    // binding 객체 미리 선언
+    private lateinit var binding : FragmentBottomSheetBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+        binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.deleteBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setMessage("프로젝트를 삭제하시겠습니까?").setPositiveButton("삭제", DialogInterface.OnClickListener{dialog, which ->
+                // BottomSheet 종료
+                dismiss()
+            }).setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->  })
+            builder.show()
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/projectodo/DetailActivity.kt
+++ app/src/main/java/com/example/projectodo/DetailActivity.kt
@@ -2,10 +2,19 @@ package com.example.projectodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectodo.databinding.ActivityDetailBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 더보기 버튼 이벤트 처리
+        binding.moreBtn.setOnClickListener {
+            val bottomSheet = BottomSheetFragment()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
     }
 }
