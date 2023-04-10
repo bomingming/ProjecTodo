c7b80a9c06a60942d0f6f6453104546bd378dba1
bomingming <byj060362@gmail.com>
2023-04-10 10:14:50 +0900

    동적 뷰 추가 시도중

--- app/src/main/java/com/example/projectodo/EditActivity.kt
+++ app/src/main/java/com/example/projectodo/EditActivity.kt
@@ -1,13 +1,77 @@
 package com.example.projectodo
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.projectodo.databinding.ActivityEditBinding
+import java.util.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var startDateString = "" // 시작일
+        var endDateString = "" // 마감일
+
+        // 시작일 버튼 이벤트
+        binding.startDateBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                startDateString = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                binding.startDateText.setText(startDateString)
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(
+                Calendar.DAY_OF_MONTH)).show()
+        }
+
+        // 마감일 버튼 이벤트
+        binding.endDateBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                endDateString = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                binding.endDateText.setText(endDateString)
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(
+                Calendar.DAY_OF_MONTH)).show()
+        }
+
+        // 수정 버튼 이벤트
+        binding.regisBtn.setOnClickListener {
+            Toast.makeText(this, "프로젝트가 수정되었습니다", Toast.LENGTH_SHORT).show()
+            finish() // 수정 화면 종료
+        }
+
+        // 취소 버튼 이벤트
+        binding.cancelBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("프로젝트 수정을 취소하시겠습니까?").setPositiveButton("확인", DialogInterface.OnClickListener{ dialog, which ->
+                Toast.makeText(this, "수정이 취소되었습니다", Toast.LENGTH_SHORT).show()
+                finish()
+            }).setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->  })
+            builder.show()
+        }
+
+        // 목표 삭제 버튼 이벤트
+        binding.deleteTargetBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("목표를 삭제하시겠습니까?").setPositiveButton("삭제", DialogInterface.OnClickListener{ dialog, which ->
+                // 목표 삭제 이벤트 구현 필요
+            }).setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->  })
+            builder.show()
+        }
+
+        // 일정 삭제 버튼 이벤트
+        binding.deleteTodoBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("일정을 삭제하시겠습니까?").setPositiveButton("삭제", DialogInterface.OnClickListener { dialog, which ->
+                // 일정 삭제 이벤트 구현 필요
+            }).setNegativeButton("취소", DialogInterface.OnClickListener{ dialog, which ->  })
+            builder.show()
+        }
     }
 }
--- app/src/main/java/com/example/projectodo/MainActivity.kt
+++ app/src/main/java/com/example/projectodo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.projectodo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.projectodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,14 +17,22 @@ class MainActivity : AppCompatActivity() {
         startActivity(loadingIntent)
 
         // 프로젝트 추가 버튼 이벤트 처리(새창)
-        binding.addBtn.setOnClickListener{
+        /*binding.addBtn.setOnClickListener{
             val addIntent: Intent = Intent(this, AddActivity::class.java)
             startActivity(addIntent)
-        }
+        }*/
 
         binding.block.setOnClickListener{
             val detailIntent: Intent = Intent(this, DetailActivity::class.java)
             startActivity(detailIntent)
+        }
+
+        val parentLayout = binding.block
+
+        binding.addBtn.setOnClickListener {
+            val inflater = LayoutInflater.from(this)
+            val view = inflater.inflate(R.layout.project_block, null)
+            parentLayout.addView(view)
         }
     }
 }
