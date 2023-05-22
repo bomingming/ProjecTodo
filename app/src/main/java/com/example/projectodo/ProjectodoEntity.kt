95d3bdd5a871118fc7940eccfd0d33bd4f79d8e9
bomingming <byj060362@gmail.com>
2023-05-22 17:42:24 +0900

    target 테이블 Entity 추가

--- app/src/main/java/com/example/projectodo/ProjectodoEntity.kt
+++ app/src/main/java/com/example/projectodo/ProjectodoEntity.kt
@@ -1,14 +1,31 @@
 package com.example.projectodo
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 // project 테이블
 @Entity(tableName = "project")
-data class ProjectodoEntity (
+data class ProjectEntity (
     @PrimaryKey(autoGenerate = true) val project_code: Int,
     val project_title: String,
     val start_day: String,
     val end_day: String
     )
+
+// target 테이블
+@Entity(tableName = "target", foreignKeys = [ForeignKey(
+    entity = ProjectEntity::class,
+    parentColumns = ["project_code"],
+    childColumns = ["project_code_for"],
+    onDelete = ForeignKey.CASCADE,
+    onUpdate = ForeignKey.CASCADE
+)])
+data class TargetEntity(
+    @PrimaryKey(autoGenerate = true) val target_code: Int,
+    val project_code_for: Int,
+    val target_title: String,
+    val todo_count: Int
+    )
+
 
