4c5a1ff7e8ffe397c520d5c397df37d31aca39b6
김보민 <byj060362@gmail.com>
2023-06-16 17:35:11 +0900

    목표 개수만큼 진행률 출력

--- app/src/main/java/com/example/projectodo/ProgressPopupActivity.kt
+++ app/src/main/java/com/example/projectodo/ProgressPopupActivity.kt
@@ -2,6 +2,11 @@ package com.example.projectodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.projectodo.databinding.ActivityDetailBinding
 import com.example.projectodo.databinding.ActivityProgressPopupBinding
 
@@ -21,12 +26,31 @@ class ProgressPopupActivity : AppCompatActivity() {
         val window = window
         window.setLayout(width, height)
 
+        val projectCode = intent.getIntExtra("프로젝트 코드", 0) // 프로젝트 코드 받아오기
+        val parentLayout = findViewById<LinearLayout>(R.id.progress_layout) // 레이아웃 객체 연결
+
+        getTarget(projectCode){ targetCount->
+            for(i in 0 until targetCount){
+                val progressView = layoutInflater.inflate(R.layout.progressbar, null)
+                if(progressView.parent != null){
+                    (progressView.parent as ViewGroup).removeView(progressView)
+                }
+                parentLayout.addView(progressView) // 목표 개수만큼 프로그레스바 동적 출력
+            }
+        }
     }
 
-    private fun getTarget(){
+    private fun getTarget(projectCode: Int, callback: (Int)-> Unit){
         Thread{
             val database = AppDatabase.getInstance(this)
             val projectDao = database?.projectDAO()
+            val targets = projectDao?.getTargetByCode(projectCode) // 프로젝트 코드로 목표 받아오기
+
+            runOnUiThread{
+                val targetCount = targets?.size
+                callback(targetCount?: 0)
+            }
+
 
         }.start()
     }
