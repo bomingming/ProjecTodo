0bb107791b2d470d08f33a0989841f1b52f1c45b
김보민 <byj060362@gmail.com>
2023-06-19 10:48:46 +0900

    상세 화면 중복 오류 해결

--- app/src/main/java/com/example/projectodo/DetailActivity.kt
+++ app/src/main/java/com/example/projectodo/DetailActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.view.setMargins
 import com.example.projectodo.databinding.ActivityDetailBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.nio.file.Files.size
+import java.util.concurrent.ThreadPoolExecutor
 
 class DetailActivity : AppCompatActivity() {
 
@@ -83,7 +84,7 @@ class DetailActivity : AppCompatActivity() {
                     // DB에 완료 값 저장(1)
                     projectDao?.editTodoCheck(todoCodeList[index], 1)
                 } else {
-                    // DB에 미완료 값 저장(2)
+                    // DB에 미완료 값 저장(0)
                     projectDao?.editTodoCheck(todoCodeList[index], 0)
                 }
             }
@@ -91,14 +92,13 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun refreshDetail(binding: ActivityDetailBinding){
+        val database = AppDatabase.getInstance(this)
+        val projectDao = database?.projectDAO()
+
+        // 프로젝트 코드를 기준으로 DB에서 값 받아오기
+        val projectCode_detail = intent.getIntExtra("프로젝트 코드", 0)
 
         Thread{
-            val database = AppDatabase.getInstance(this)
-            val projectDao = database?.projectDAO()
-
-            // 프로젝트 코드를 기준으로 DB에서 값 받아오기
-            val projectCode_detail = intent.getIntExtra("프로젝트 코드", 0)
-
             val project = projectDao?.getProjectByCode(projectCode_detail) // 프로젝트 코드로 프로젝트 값 받아오기
             val itemTarget = projectDao?.getTargetByCode(projectCode_detail) // 프로젝트 코드로 목표 목록 받아오기
 
@@ -123,7 +123,7 @@ class DetailActivity : AppCompatActivity() {
                         layoutParams.setMargins(0, 10, 0, 40)
                         view.layoutParams = layoutParams
 
-                        parentLayout.addView(view)
+                        parentLayout.addView(view) // 목표 블록 추가
 
                         dynamicTarget = view.findViewById(R.id.target_title_detail) // 목표 블록 객체 할당
                         dynamicTarget?.text = item.target_title // 목표 이름 출력
@@ -132,29 +132,29 @@ class DetailActivity : AppCompatActivity() {
                             val itemTodo = projectDao.getTodoByCode(targetCode) // 목표 코드로 일정 목록 받아오기
 
                             runOnUiThread{
-                                val superParentLayout = findViewById<LinearLayout>(R.id.tg_block_detail_layout)
+                                val targetBlockLayout = parentLayout.getChildAt(i) as ConstraintLayout
+                                val tdBlockDetailParentLayout = targetBlockLayout.findViewById<LinearLayout>(R.id.td_block_detail_layout)
+                                tdBlockDetailParentLayout.removeAllViews() // 일정 레이아웃 초기화
 
                                 if(itemTodo != null){
                                     for(j in 0 until itemTodo!!.size){
                                         val tdBlockDetailLayout = inflater.inflate(R.layout.todo_block_detail, null)
                                         val item_for_todo = itemTodo[j]
 
-                                        if(tdBlockDetailLayout.parent != null){
-                                            (tdBlockDetailLayout.parent as ViewGroup).removeView(tdBlockDetailLayout)
-                                        }
-                                        val targetBlockLayout = superParentLayout.getChildAt(i) as ConstraintLayout
-                                        val tdBlockDetailParentLayout = targetBlockLayout.findViewById<LinearLayout>(R.id.td_block_detail_layout)
-                                        tdBlockDetailParentLayout.addView(tdBlockDetailLayout)
-
                                         val checkbox = tdBlockDetailLayout.findViewById<CheckBox>(R.id.todo_check_detail)
                                         // 체크박스를 리스트에 추가
                                         checkBoxList.add(checkbox)
+
                                         // 일정 코드를 리스트에 추가
                                         todoCodeList.add(item_for_todo.todo_code)
 
                                         dynamicTodo = tdBlockDetailLayout.findViewById(R.id.todo_list_detail)
                                         dynamicTodo?.text = item_for_todo.todo_detail
 
+                                        if(tdBlockDetailLayout.parent != null){
+                                            (tdBlockDetailLayout.parent as ViewGroup).removeView(tdBlockDetailLayout)
+                                        }
+                                        tdBlockDetailParentLayout.addView(tdBlockDetailLayout) // 일정 블록 추가
                                     }
                                 }
                             }
